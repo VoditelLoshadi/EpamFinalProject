a822b338daea5b00e0019494337e74000f6a60db
Aleksandr Yutushui <yutaleksandr18@gmail.com>
2023-02-28 21:05:37 +0200

    final

--- selection-committee/src/main/java/ua/epam/elearn/selection/committee/controller/command/Command.java
+++ selection-committee/src/main/java/ua/epam/elearn/selection/committee/controller/command/Command.java
@@ -2,7 +2,7 @@ package ua.epam.elearn.selection.committee.controller.command;
 
 
 import jakarta.servlet.http.HttpServletRequest;
-
+//
 public interface Command {
 
     String execute(HttpServletRequest request);
